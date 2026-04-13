f30d9a0fdd317762615200478178ff64266e4716
sre-yes-v <sreyesv24@gmail.com>
2026-04-14 00:25:32 +0530

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,13 +11,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${siteUrl}about`,
+      url: `${siteUrl}about/`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
     {
-      url: `${siteUrl}projects`,
+      url: `${siteUrl}projects/`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
